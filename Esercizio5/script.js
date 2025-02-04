bf3017b0ed7de8efb51bba95d372df22b59b2869
saccomando-pietro <pietro.saccomando@gmail.com>
2025-02-04 11:44:52 +0100

    quinto esercizio

---
+++ Esercizio5/script.js
@@ -0,0 +1,9 @@
+// Hai due array di iscritti a un corso. Usando lo spread operator, crea un nuovo array che contenga tutti gli elementi di entrambi gli array.
+// Unire iscrittiOnline e iscrittiOffline usando lo spread operator
+// OUTPUT ATTESO: ['Gino', 'Pino', 'Roberto', 'Franca', 'Salvo', 'Maria']
+
+const iscrittiOnline = ['Gino', 'Pino', 'Roberto'];
+const iscrittiOffline = ['Franca', 'Salvo', 'Maria'];
+
+let iscrittiTotali = [...iscrittiOnline, ...iscrittiOffline]
+console.log(iscrittiTotali)
